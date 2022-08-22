b263fdf03c8155289bb4100b7e9af0d89cdfb826
guuhgst <gustavoprogramador18@gmail.com>
2022-08-22 04:38:12 -0300

    feat: :package: melhorando o accordion

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,9 @@ function accordion() {
     const activeClass = 'active';
 
     if (accordionList.length) {
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
         function activeAccordion() {
             this.classList.toggle(activeClass);
             this.nextElementSibling.classList.toggle(activeClass);
